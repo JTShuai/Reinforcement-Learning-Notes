8435d4e43f27967691a5e83b379d885905b9fe54
shuai_Macbook <shuaijt1110@gmail.com>
2021-11-12 00:21:27 +0100

    add: main.py & rl.py

--- robotic_arm/main.py
+++ robotic_arm/main.py
@@ -3,38 +3,66 @@
 from .env import ArmEnv
 from .rl import DDPG
 
-# 设置全局变量
-MAX_EPISODES = 500
-MAX_EP_STEPS = 200
+"""
+Make it more robust.
+Stop episode once the finger stop at the final position for 50 steps.
+Feature & reward engineering.
+"""
+from env import ArmEnv
+from rl import DDPG
 
-# 设置环境
+MAX_EPISODES = 900
+MAX_EP_STEPS = 200
+ON_TRAIN = True
+
+# set env
 env = ArmEnv()
 s_dim = env.state_dim
 a_dim = env.action_dim
 a_bound = env.action_bound
 
-# 设置学习方法 (这里使用 DDPG)
+# set RL method (continuous)
 rl = DDPG(a_dim, s_dim, a_bound)
 
-# 开始训练
-for i in range(MAX_EPISODES):
-    # 初始化回合设置
+steps = []
+def train():
+    # start training
+    for i in range(MAX_EPISODES):
+        s = env.reset()
+        ep_r = 0.
+        for j in range(MAX_EP_STEPS):
+            # env.render()
+
+            a = rl.choose_action(s)
+
+            s_, r, done = env.step(a)
+
+            rl.store_transition(s, a, r, s_)
+
+            ep_r += r
+            if rl.memory_full:
+                # start to learn once has fulfilled the memory
+                rl.learn()
+
+            s = s_
+            if done or j == MAX_EP_STEPS-1:
+                print('Ep: %i | %s | ep_r: %.1f | step: %i' % (i, '---' if not done else 'done', ep_r, j))
+                break
+    rl.save()
+
+
+def eval():
+    rl.restore()
+    env.render()
+    env.viewer.set_vsync(True)
     s = env.reset()
-
-    for j in range(MAX_EP_STEPS):
-        # 环境的渲染
+    while True:
         env.render()
-        # RL 选择动作
         a = rl.choose_action(s)
-        # 选取的动作与环境交互，得到 observation, reward, done
-        s_, r, done = env.step(a)
+        s, r, done = env.step(a)
 
-        # DDPG 这种强化学习需要存放记忆库
-        rl.store_transition(s, a, r, s_)
 
-        if rl.memory_full:
-            # 记忆库满了, 开始学习
-            rl.learn()
-
-        # 变为下一回合
-        s = s_
+if ON_TRAIN:
+    train()
+else:
+    eval()
